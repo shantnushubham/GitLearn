0e535d3d852bf486c0a4f4db77467b51fa2a3f5e
Shantanu Shubham <shantanu0707@gmail.com>
2023-04-28 19:26:34 +0530

    My First Commit

---
+++ myFile.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter 1st number."))
+num2 = int(input("Enter 2nd number."))
+
+
+def add_two_numbers(a: int, b: int) -> None:
+    return a + b
+
+
+print(add_two_numbers(num1, num2))
